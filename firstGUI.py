6f2bae560d8b752dbf49046f418165acaecd92d5
pirate <lalitnegi1997@gmail.com>
2019-04-24 01:40:22 +0530

    first desktop application

---
+++ firstGUI.py
@@ -0,0 +1,27 @@
+# A POPUP ALERT NOTIFICATION PROGRAM USING PYQT5
+import sys
+import time
+from PyQt5.QtCore import * #QtCore module contains non-GUI functionality for working with file and directory etc.
+from PyQt5.QtWidgets import *
+
+app = QApplication(sys.argv) #sys.srgs are command line arguments
+
+due = input("Enter Time for Alert : ")
+message = input("Enter Message for Alert : ")
+
+try:
+	hours, mins = due.split(":")
+	due = QTime(int(hours), int(mins))
+	if not due.isValid():
+		raise ValueError
+except ValueError:
+	message = "Time entered is not a valid time"
+
+while QTime.currentTime() < due:
+	pass
+
+label = QLabel("<font color=red size=72><b>" + message + "</b></font>")	
+label.setWindowFlags(Qt.SplashScreen)
+label.show() #show the window
+QTimer.singleShot(6000, app.quit)
+sys.exit(app.exec_())
